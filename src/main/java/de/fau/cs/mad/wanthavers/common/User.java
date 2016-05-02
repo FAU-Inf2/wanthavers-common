3a8c62ab15ffb57d14c93efeeadd2a8a056a4ef1
H0schi <thorsten-isaac@gmx.net>
2016-05-02 16:25:40 +0200

    added initial User and UserResource, build.gradle dependency javax.persistence

--- src/main/java/de/fau/cs/mad/wanthavers/common/User.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/User.java
@@ -1,5 +1,60 @@
 package de.fau.cs.mad.wanthavers.common;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) //TODO: change to UUID
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Date birthday;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @JsonProperty
+    public long getID() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @JsonProperty
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
--- src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/UserResource.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/UserResource.java
@@ -1,11 +1,19 @@
 package de.fau.cs.mad.wanthavers.common.rest.api;
 
+import de.fau.cs.mad.wanthavers.common.User;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("users")
 @Api(value = "users", description = "Operations on users")
 public interface UserResource {
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    User get();
 
 }
