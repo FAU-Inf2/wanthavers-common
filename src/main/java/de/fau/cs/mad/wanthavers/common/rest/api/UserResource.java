238d6292d1309a0f64cab96720b2c490352b3a50
Nicolas Wolf <nicolas.wolf@fau.de>
2016-05-02 20:51:28 +0200

    extended UserResource by post, put, delete

--- src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/UserResource.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/UserResource.java
@@ -1,19 +1,65 @@
 package de.fau.cs.mad.wanthavers.common.rest.api;
 
 import de.fau.cs.mad.wanthavers.common.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("users")
-@Api(value = "users", description = "Operations on users")
+@Path("v1/users")
+@Api(value = "v1/users", description = "Operations on users")
 public interface UserResource {
 
     @GET
-    @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
     User get();
+
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Find user by id",
+            notes = "Returns a user if it exists.",
+            response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retrieved user in body"),
+            @ApiResponse(code = 400, message = "Invalid id supplied"),
+            @ApiResponse(code = 404, message = "User not found"),
+            @ApiResponse(code = 500, message = "Server broken, please contact administrator")})
+    User get(
+            @ApiParam(value = "id of the desired user", required = true)
+            @PathParam("id")long id);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Create a new user",
+            notes = "Inserts a new user into the database. Returns the new user object including id.",
+            response = User.class)
+    User createUser(@ApiParam(value = "User to create", required = true)User newUser);
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Update an existing user",
+            notes = "Updates the details of an existing user. Returns the updated user object.",
+            response = User.class)
+    User updateUser(
+            @ApiParam(value = "id of the user", required = true)
+            @PathParam("id") long id,
+            @ApiParam(value = "new details of the specified user", required = true) User user);
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @ApiOperation(
+            value = "Delete a user",
+            notes = "Removes a user from the database.")
+    void deleteUser(
+            @ApiParam(value = "id of the to be deleted user", required = true)
+            @PathParam("id") long id);
 
 }
