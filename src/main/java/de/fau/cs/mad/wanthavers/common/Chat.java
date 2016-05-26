2b88840d71d31a78830812e888ad261b80bde6f3
Nicolas Wolf <nicolas.wolf@fau.de>
2016-05-27 00:58:11 +0200

    added user and desire objects to chat

--- src/main/java/de/fau/cs/mad/wanthavers/common/Chat.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/Chat.java
@@ -3,13 +3,16 @@ package de.fau.cs.mad.wanthavers.common;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
-import java.util.Date;
 
 
 public class Chat extends AbstractParseModel implements Serializable {
     private long user1;
     private long user2;
     private long desireId;
+
+    private User userObject1;
+    private User userObject2;
+    private Desire desire;
 
 
     @JsonProperty
@@ -39,5 +42,30 @@ public class Chat extends AbstractParseModel implements Serializable {
         this.desireId = desireId;
     }
 
+    @JsonProperty
+    public User getUserObject1() {
+        return userObject1;
+    }
 
+    public void setUserObject1(User userObject1) {
+        this.userObject1 = userObject1;
+    }
+
+    @JsonProperty
+    public User getUserObject2() {
+        return userObject2;
+    }
+
+    public void setUserObject2(User userObject2) {
+        this.userObject2 = userObject2;
+    }
+
+    @JsonProperty
+    public Desire getDesire() {
+        return desire;
+    }
+
+    public void setDesire(Desire desire) {
+        this.desire = desire;
+    }
 }
