84cac553d17f06a0a64a4b8a6aaf374b2d8af936
Nicolas Wolf <nicolas.wolf@fau.de>
2016-06-02 17:35:38 +0200

    only one category

--- src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/DesireResource.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/DesireResource.java
@@ -48,7 +48,7 @@ public interface DesireResource {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Retrieved list of desires (may be empty)"),
             @ApiResponse(code = 500, message = "Server broken, please contact administrator")})
-    List<Desire> getByFilters(@QueryParam("categories") List<Long> categories, @QueryParam("price_min") double price_min, @QueryParam("price_max") double price_max,
+    List<Desire> getByFilters(@QueryParam("category") long category, @QueryParam("price_min") double price_min, @QueryParam("price_max") double price_max,
                               @QueryParam("reward_min") double reward_min, @QueryParam("rating_min") double rating_min, @QueryParam("lat") double lat, @QueryParam("lon") double lon, @QueryParam("radius") double radius);
 
     @GET
