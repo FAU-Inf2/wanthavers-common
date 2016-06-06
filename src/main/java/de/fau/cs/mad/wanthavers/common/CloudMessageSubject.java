0c48f1f71fd69839c86324aa921fa54268f68fe4
H0schi <thorsten-isaac@gmx.net>
2016-06-06 17:28:58 +0200

    added SubjectClass for access from server and app

---
+++ src/main/java/de/fau/cs/mad/wanthavers/common/CloudMessageSubject.java
@@ -0,0 +1,7 @@
+package de.fau.cs.mad.wanthavers.common;
+
+
+public class CloudMessageSubject {
+    public static String NEWMESSAGE = "NewMessage";
+    public static String DESIREUPDATE = "DesireUpdate";
+}
