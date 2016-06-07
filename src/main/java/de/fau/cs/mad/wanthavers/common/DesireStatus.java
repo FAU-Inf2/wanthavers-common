2c5bde036275b8a0072a3f70a08b71b469b025df
philipp <philsippl@gmail.com>
2016-06-07 09:53:49 +0200

    made status final

--- src/main/java/de/fau/cs/mad/wanthavers/common/DesireStatus.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/DesireStatus.java
@@ -1,8 +1,8 @@
 package de.fau.cs.mad.wanthavers.common;
 
 public final class DesireStatus {
-    public static int STATUS_DELETED = 0;
-    public static int STATUS_OPEN = 1;
-    public static int STATUS_IN_PROGRESS = 2;
-    public static int STATUS_DONE = 3;
+    public static final int STATUS_DELETED = 0;
+    public static final int STATUS_OPEN = 1;
+    public static final int STATUS_IN_PROGRESS = 2;
+    public static final int STATUS_DONE = 3;
 }
--- src/main/java/de/fau/cs/mad/wanthavers/common/HaverStatus.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/HaverStatus.java
@@ -1,8 +1,8 @@
 package de.fau.cs.mad.wanthavers.common;
 
 public final class HaverStatus {
-    public static int ADDED = 0;
-    public static int ACCEPTED = 1;
-    public static int REJECTED = 2;
-    public static int DELETED = 3;
+    public static final int ADDED = 0;
+    public static final int ACCEPTED = 1;
+    public static final int REJECTED = 2;
+    public static final int DELETED = 3;
 }
