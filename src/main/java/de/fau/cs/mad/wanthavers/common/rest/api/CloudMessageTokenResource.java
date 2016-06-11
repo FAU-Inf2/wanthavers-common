09d64bdf0c7d0a3ca020932b049d1db4683045d2
Nicolas Wolf <nicolas.wolf@fau.de>
2016-06-11 16:03:49 +0200

    use auth for cloud message tokens

--- src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/CloudMessageTokenResource.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/CloudMessageTokenResource.java
@@ -2,13 +2,15 @@ package de.fau.cs.mad.wanthavers.common.rest.api;
 
 
 import de.fau.cs.mad.wanthavers.common.CloudMessageToken;
+import de.fau.cs.mad.wanthavers.common.User;
+import io.dropwizard.auth.Auth;
 import io.swagger.annotations.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("v1/users/{user-id}/tokens")
+@Path("v1/users/tokens")
 @Api(value = "v1/users/{user-id}/tokens", description = "Operations on CloudMessageTokens")
 public interface CloudMessageTokenResource {
 
@@ -16,7 +18,7 @@ public interface CloudMessageTokenResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(
-            value = "Find tokens by user's id",
+            value = "Find tokens by auth user",
             notes = "Returns tokens of an existing user.",
             response = List.class)
     @ApiResponses(value = {
@@ -25,8 +27,8 @@ public interface CloudMessageTokenResource {
             @ApiResponse(code = 404, message = "User not found"),
             @ApiResponse(code = 500, message = "Server broken, please contact administrator")})
     List<CloudMessageToken> getAllTokens(
-            @ApiParam(value = "id of the desired user", required = true)
-            @PathParam("user-id")long userId);
+            @ApiParam(value = "auth user", required = true)
+            @Auth User user);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -36,8 +38,8 @@ public interface CloudMessageTokenResource {
             notes = "Inserts a new token into the database. Returns the new CloudMessageToken object including id.",
             response = CloudMessageToken.class)
     CloudMessageToken createToken(
-            @ApiParam(value = "id of the desired user", required = true)
-            @PathParam("user-id")long userId,
+            @ApiParam(value = "auth user", required = true)
+            @Auth User user,
             @ApiParam(value = "Token to create", required = true)CloudMessageToken newToken
     );
 
@@ -55,8 +57,8 @@ public interface CloudMessageTokenResource {
             @ApiResponse(code = 404, message = "Token not found"),
             @ApiResponse(code = 500, message = "Server broken, please contact administrator")})
     CloudMessageToken get(
-            @ApiParam(value = "id of the desired user", required = true)
-            @PathParam("user-id")long userId,
+            @ApiParam(value = "auth user", required = true)
+            @Auth User user,
             @ApiParam(value = "id of the token", required = true)
             @PathParam("id") long id);
 
@@ -69,8 +71,8 @@ public interface CloudMessageTokenResource {
             notes = "Updates the details of an existing token. Returns the updated CloudMessageToken object.",
             response = CloudMessageToken.class)
     CloudMessageToken updateToken(
-            @ApiParam(value = "id of the desired user", required = true)
-            @PathParam("user-id")long userId,
+            @ApiParam(value = "auth user", required = true)
+            @Auth User user,
             @ApiParam(value = "id of the token", required = true)
             @PathParam("id") long id,
             @ApiParam(value = "new details of the specified token", required = true) CloudMessageToken token);
@@ -82,8 +84,8 @@ public interface CloudMessageTokenResource {
             value = "Delete a CloudMessageToken",
             notes = "Removes a token from the database.")
     void deleteToken(
-            @ApiParam(value = "id of the desired user", required = true)
-            @PathParam("user-id")long userId,
+            @ApiParam(value = "auth user", required = true)
+            @Auth User user,
             @ApiParam(value = "id of the to be deleted token", required = true)
             @PathParam("id") long id);
 
