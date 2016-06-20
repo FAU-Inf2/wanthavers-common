a91c519dc6909cc19bc97986ba019cf46fd19e57
H0schi <thorsten-isaac@gmx.net>
2016-06-20 12:03:57 +0200

    added strings for cloud message keys

--- src/main/java/de/fau/cs/mad/wanthavers/common/CloudMessageSubject.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/CloudMessageSubject.java
@@ -2,6 +2,26 @@ package de.fau.cs.mad.wanthavers.common;
 
 
 public class CloudMessageSubject {
+    /*
+        Structure of com.google.firebase.messaging.RemoteMessage (JSON):
+        {
+            "to": "<token>",
+            "notification": {
+                "body": ...,
+                "title": ...
+                "icon" ...
+            }
+            "data": {
+                "subject": <subject>
+                ["sender": "Yoda" (optional)]
+            }
+        }
+     */
+
     public static String NEWMESSAGE = "NewMessage";
+    public static String NEWMESSAGE_SENDER = "sender";
+    public static String NEWMESSAGE_SENDERID = "senderId";
+    public static String NEWMESSAGE_CHATID = "chatId";
+
     public static String DESIREUPDATE = "DesireUpdate";
 }
