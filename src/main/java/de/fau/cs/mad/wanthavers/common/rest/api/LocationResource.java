1f93ff57f541397f72ea7dbf1f09d3662a168774
Nicolas Wolf <nicolas.wolf@fau.de>
2016-06-24 20:08:45 +0200

    added getLocationById

--- src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/LocationResource.java
+++ src/main/java/de/fau/cs/mad/wanthavers/common/rest/api/LocationResource.java
@@ -1,13 +1,14 @@
 package de.fau.cs.mad.wanthavers.common.rest.api;
 
-import de.fau.cs.mad.wanthavers.common.*;
+import de.fau.cs.mad.wanthavers.common.Location;
+import de.fau.cs.mad.wanthavers.common.User;
 import io.dropwizard.auth.Auth;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
-import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 @Path("v1/locations")
 @Api(value = "v1/locations", description = "Operations on locations")
@@ -35,6 +36,16 @@ public interface LocationResource {
             @ApiParam(value = "Location to create", required = true) Location location
     );
 
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Get an existing location",
+            notes = "Gets the details of an existing location.",
+            response = Location.class)
+    Location get(@Auth User user, @PathParam("id") long id);
+
     @PUT
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -44,8 +55,8 @@ public interface LocationResource {
             notes = "Updates the details of an existing location. Returns the updated location object.",
             response = Location.class)
     Location updateLocation(@Auth User user,
-                          @ApiParam(value = "id of the location", required = true) @PathParam("id") long id,
-                          @ApiParam(value = "Location to create", required = true) Location location);
+                            @ApiParam(value = "id of the location", required = true) @PathParam("id") long id,
+                            @ApiParam(value = "Location to create", required = true) Location location);
 
     @DELETE
     @Path("/{id}")
